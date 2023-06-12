27f3dfce51f8e024fd61183b9fb6512591a899fa
programist78 <pavelmolodecfda@gmail.com>
2023-06-12 11:32:18 +0300

    + 50% homepage, part 4

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -29,9 +29,9 @@ console.log(`admin: ${admin}`)
 console.log(`people: ${people}`)
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <>
-        <NavBar />
+        {/* <NavBar /> */}
         {children}
         <Footer />
       </>
